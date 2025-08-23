5cc322b0b28c564c75e49da0aba24d492ec467a7
AshithaSujith <ashithasujith8@gmail.com>
2025-08-23 13:29:18 +0530

    add

--- test_pandas.py
+++ test_pandas.py
@@ -6,15 +6,28 @@ import pandas as pd
 # df=pd.DataFrame(data)
 # print(df)
 
-data={'Name':['Alice','Bob','Charlie','David','Eva','Frank'],
-      'score':[85,90,78,92,88,75],
+# data={'Name':['Alice','Bob','Charlie','David','Eva','Frank'],
+#       'score':[85,90,78,92,88,75],
+# }
+# df=pd.DataFrame(data)
+# print(df.head())
+# print(df.head(3))
+# print(df.tail())
+# print(df.tail(3))
+# df.info()
+# print(df.describe())
+# print(df.columns)
+# print(df.shape)
+
+data = {
+    'name': ['Alice', 'bob', 'charlie'],
+    'age': [25, 30, 35],
+    'city': ['New York', 'Los Angeles', 'Chicago']
 }
-df=pd.DataFrame(data)
-print(df.head())
-print(df.head(3))
-print(df.tail())
-print(df.tail(3))
-df.info()
-print(df.describe())
-print(df.columns)
-print(df.shape)
+
+df = pd.DataFrame(data, index=['a', 'b', 'c'])
+print(df)  
+
+print(df.loc['a'])  
+print(df.loc['b', 'name']) 
+print(df.loc[:, ['name', 'city']])  
