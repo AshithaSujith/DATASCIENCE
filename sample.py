ddc443e015602e46aa7ae7ed148d0de6252a24b0
AshithaSujith <ashithasujith8@gmail.com>
2025-08-23 11:07:35 +0530

    add

---
+++ sample.py
@@ -0,0 +1,25 @@
+# color = input("What is your favorite color? ")
+# print("Your favorite color is", color + "!")
+
+# count = 0
+# while count < 5:
+#     print("Count is", count)
+#     count += 1
+
+
+# for i in range(5):  # range(5) = 0,1,2,3,4
+#     print(i)
+
+# for i in range(1,11):
+#     print(i)
+
+# count=1
+# while count<=10:
+#     print("count is",count)
+#     count=count+1
+
+# fruits=["apple","mango","banana"]
+# for fruit in fruits:
+#         print(fruit)
+
+count=10
