1e87ed92149f7da0011b1c38e8700687e5617ae8
AshithaSujith <ashithasujith8@gmail.com>
2025-08-23 10:04:40 +0530

    add this

---
+++ emoji.py
@@ -0,0 +1,6 @@
+emotions={"happy":"😊","sad":"😢","angry":"😠","love":"❤️","surprised":"😲","confused":"😕","bored":"😐","tired":"😴","excited":"🤩","nervous":"😬"}
+print(emotions)
+text=input("Enter your message: ").strip()
+for word, emoji in emotions.items():
+    text = text.replace(word, emoji)
+print("converted message:",text)
